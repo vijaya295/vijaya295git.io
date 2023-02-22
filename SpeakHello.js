b3b84173c88cefa2bc212158b993c86a4bf9c1ea
vijaya295 <107910026+vijaya295@users.noreply.github.com>
2023-02-22 17:50:40 -0600

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,7 +1,6 @@
 //STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
-
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
