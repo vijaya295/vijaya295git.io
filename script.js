fbc27767fab62c9195e1f47f57147b2d0ce591f8
vijaya295 <107910026+vijaya295@users.noreply.github.com>
2023-02-22 17:54:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// ******************************* 
+// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
 // Module 4 Assignment Instructions.
 //
 // The idea of this assignment is to take an existing array of names
@@ -40,3 +44,5 @@ for (var i = 0; i < names.length; i++) {
 }
 
 })();
+
+
