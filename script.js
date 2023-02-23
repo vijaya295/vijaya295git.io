da9505a61ebe57303f79310c50cb6138dcab906c
vijaya295 <107910026+vijaya295@users.noreply.github.com>
2023-02-22 18:06:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -32,10 +32,9 @@ for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
   }
 }
 
